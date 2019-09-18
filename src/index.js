87474402069abf47dc5ae8669832336d56243d3e
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-18 14:15:11 -0700

    Initial app setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+// querySelector is telling react where to render app
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
