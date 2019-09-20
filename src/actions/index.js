e74269519a863f8f71d9233e8d21fe23774e0c10
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-20 14:55:28 -0700

    installed lodash and used memoize function for fetchUser action creator to make sure we get only 1 request for the same userId

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 //defining a function that will return a function
@@ -8,8 +9,12 @@ export const fetchPosts = () => async dispatch => {
 };
 
 //creating new action creator
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: "FETCH_User", payload: response.data });
-};
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
